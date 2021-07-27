d02ef086e2007d7e593095849f356861a1178bcc
Sachman19 <sachindmishra@knights.ucf.edu>
2021-07-27 03:04:57 -0400

    created export class

--- src/main/java/ucf/assignments/export.java
+++ src/main/java/ucf/assignments/export.java
@@ -5,5 +5,28 @@
 
 package ucf.assignments;
 
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class export {
+    public void exportTSV(ArrayList<item> itemList) {
+        try {
+            File export = new File("export.tsv");
+            if (export.createNewFile()) {
+                PrintWriter outFile = new PrintWriter(export);
+                outFile.print("Price%tSerial%tName%n");
+                for(int i = 0; i < itemList.size(); i++){
+                    outFile.format("%.5f/t", itemList.get(i).getPrice());
+                    outFile.format("%.12s/t", itemList.get(i).getSerial());
+                    outFile.format("%.12s/n", itemList.get(i).getName());
+                }
+            }
+        } catch (IOException e) {
+            return;
+        }
+
+    }
 }
