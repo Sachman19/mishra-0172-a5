a05ee7c6b4f23773bcbe70ea2734c3547929a04b
Sachman19 <sachindmishra@knights.ucf.edu>
2021-07-28 04:43:27 -0400

    import functions finished

--- src/main/java/ucf/assignments/importer.java
+++ src/main/java/ucf/assignments/importer.java
@@ -53,35 +53,12 @@ public class importer {
         File importFile = new File(filename);
         if(importFile.exists()){
             Scanner inFile = new Scanner(importFile);
+            inFile.nextLine();
             while(inFile.hasNextLine()){
-
-            }
-
-            while(inFile.hasNextLine()){
-                String input;
-                if(inFile.nextLine().equalsIgnoreCase("<tr>")){
-                    item newItem = new item();
-                    for(int i = 0; i < 3; i++){
-                        if(i == 0){
-                            input = inFile.nextLine();
-                            input.replace("<td>", "");
-                            input.replace("</td>", "");
-                            newItem.setPrice(Double.parseDouble(input));
-                        }
-                        if(i == 1){
-                            input = inFile.nextLine();
-                            input.replace("<td>", "");
-                            input.replace("</td>", "");
-                            newItem.setSerial(input);
-                        }
-                        if(i == 2){
-                            input = inFile.nextLine();
-                            input.replace("<td>", "");
-                            input.replace("</td>", "");
-                            newItem.setName(input);
-                        }
-                    }
-                }
+                String[] input = inFile.nextLine().split("\t");
+                item newItem = new item();
+                newItem.setInfo(input[1], input[2], Double.parseDouble(input[0]));
+                returnList.add(newItem);
             }
             inFile.close();
         }
