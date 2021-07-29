5e8123811ad9188ca456283ea652d9c8d8b9b3b7
Sachman19 <sachindmishra@knights.ucf.edu>
2021-07-28 21:16:21 -0400

    item menu fxml controller added

--- src/main/java/ucf/assignments/controllerMenu.java
+++ src/main/java/ucf/assignments/controllerMenu.java
@@ -1,9 +1,27 @@
 package ucf.assignments;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class controllerMenu {
+
+    public TableColumn tablePrice;
+    public TableColumn tableSerial;
+    public TableColumn tableName;
+    public MenuItem importTSVButton;
+    public MenuItem importHTMLButton;
+    public MenuItem exportTSVButton;
+    public MenuItem exportHTMLButton;
+    public MenuItem sortNameButton;
+    public MenuItem sortSerialButton;
+    public MenuItem sortPriceButton;
+    public MenuItem readMeButton;
+    public Button addButton;
+    public TextField searchBar;
 
     public void editItemMenu(MouseEvent mouseEvent) {
     }
---
+++ src/main/java/ucf/assignments/itemController.java
@@ -0,0 +1,17 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class itemController {
+    public TextField serialField;
+    public TextField priceInput;
+    public TextField nameField;
+    public Button deleteItemButton;
+    public Button saveItemButton;
+    public Button returnButton;
+
+    public void closeWindow(ActionEvent actionEvent) {
+    }
+}
